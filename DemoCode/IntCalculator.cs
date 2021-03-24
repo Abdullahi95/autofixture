a38e921a49a39d962facdbe3fe2bbd9720180d08
Hassan822 <Hassan931221@gmail.com>
2021-03-24 16:39:20 +0000

    Add project files.

---
+++ DemoCode/IntCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    class IntCalculator
+    {
+        public int Value { get; private set; }
+
+        public void Subtract(int number)
+        {
+            Value -= number;
+        }
+
+        public void Add(int number)
+        {
+            Value += number;
+        }
+
+    }
+}
